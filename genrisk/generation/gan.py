3af881efdfe8fc7456e6a12ad8ec707df5a15e71
andrew_work <anpotapov_5@edu.hse.ru>
2025-06-22 17:07:00 +0300

    Добавил недостающие файлы

--- genrisk/generation/gan.py
+++ genrisk/generation/gan.py
@@ -17,13 +17,48 @@ class GANModule(LightningModule):
         self.lr = lr
         self.num_disc_steps = num_disc_steps
 
+    import torch
+    import torch.nn.functional as F
+
+    def vectorized_autocorrelation(self, fake_gen, lag=300, window_size=65):
+        """
+        Устойчивый расчет автокорреляции для 5D рядов
+        Args:
+            fake_gen: [batch_size, num_assets, seq_len]
+            lag: лаг L (должен удовлетворять lag + window_size <= seq_len)
+            window_size: N
+        Returns:
+            [batch_size, num_assets] (усредненная по окнам автокорреляция)
+        """
+        x = fake_gen.permute(0, 2, 1)
+        # 2. Создаем скользящие окна
+        windows = x.unfold(2, window_size, 1)  # [batch, assets, num_windows, window_size]
+
+
+        # 3. Центрирование данных
+        windows_centered = windows - windows.mean(dim=3, keepdim=True)
+
+        # 4. Вычисление корреляции
+        x_t = windows_centered[:, :, 0]
+        x_t_lag = windows_centered[:, :, lag - window_size]
+        # 5. Устойчивые вычисления
+        cov = (x_t * x_t_lag).sum(dim=2)
+        std_t = torch.norm(x_t, dim=2)
+        std_t_lag = torch.norm(x_t_lag, dim=2)
+
+        autocorr = cov / (std_t * std_t_lag)
+
+        return (autocorr.mean(axis=0) ** 2 - (torch.ones((5)) * 0.7) ** 2).mean()
+
     def disc_loss(self, real_logits, fake_logits):
         real_is_real = torch.log(torch.sigmoid(real_logits) + 1e-10)
         fake_is_fake = torch.log(1 - torch.sigmoid(fake_logits) + 1e-10)
         return -(real_is_real + fake_is_fake).mean() / 2
     
-    def gen_loss(self, fake_logits):
+    def gen_loss(self, fake_logits, fake):
         fake_is_real = torch.log(torch.sigmoid(fake_logits) + 1e-10)
+
+        # corr_loss = self.vectorized_autocorrelation(fake)
         return -fake_is_real.mean()
 
 
@@ -46,7 +81,7 @@ class GANModule(LightningModule):
         
         fake = self.gen(z, cond)
         fake_logits, _ = self.disc(fake, cond)
-        g_loss = self.gen_loss(fake_logits)
+        g_loss = self.gen_loss(fake_logits, fake)
 
         gen_opt.zero_grad()
         self.manual_backward(g_loss)
